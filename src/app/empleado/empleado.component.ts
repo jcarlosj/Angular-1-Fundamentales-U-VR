a26e8bd8fc4a394c5b399ce6df9b358ade36c9d4
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-04-13 14:08:10 -0500

    Crea componente 'empleados' despliega en la vista principal

---
+++ src/app/empleado/empleado.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-empleado',
+    templateUrl: './empleado.component.html',
+    styleUrls: [ './empleado.component.css' ]
+})
+export class EmpleadoComponent {
+    /* Propiedades (Atributos) */
+    public nombre_componente = 'Componente de Empleado';
+    public listado_empleados = 'Jhonny, Juan, Bryan, Maura', 'Charles', 'Melisa';
+
+}
