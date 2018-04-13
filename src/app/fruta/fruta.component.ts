f022a64f70050e2471fe1f311c632452b705fd59
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-04-13 14:37:25 -0500

    Visibilidad de las propiedades en 'TypeScript' (public/private/protected)

--- src/app/fruta/fruta.component.ts
+++ src/app/fruta/fruta.component.ts
@@ -7,9 +7,12 @@ import { Component } from '@angular/core';
 })
 export class FrutaComponent {
     /* Propiedades (Atributos) */
-    public nombre_componente = 'Componente de fruta';
-    public listado_frutas = 'Mango, Guanabana, Fresa, Cereza, Papaya';
-    /* NOTA: Para poder visualizar las propiedades de la clase en la plantilla
-             Angular se vale de las llaves dobles {{ }} para desplegarlas, esto
-             se conoce con el nombre de "Binding por interpolación" */
+    nombre_componente = 'Componente de fruta';                  // Si no se define el tipo de visibilidad se asume que es 'public'
+    public listado_frutas_acidas = 'Bergamota, Clementina, Cidra, Lima, Limón, Mandarina, Naranja';
+    private listado_frutas_neutras = 'Aceituna, Avellana, Coco, Maní, Cacao, Aguacate, Castaña, Almendra, Nuez y Macadamia';
+    protected listado_frutas_dulces = 'Melón, Plátano, Sandía, Manzana Golden, Ciruelas, Uvas, Albaricoque, Chirimoya, Caqui, Grosella, Granada';
+
+    /* NOTA: La visibilidad de cada una de las propiedades solo funcionan de cara
+             a otras clases, no será tenida en cuenta por TypeScript al desplegarla
+             en vista del componente */
 }
