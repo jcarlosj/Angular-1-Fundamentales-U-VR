3b9f98352c81fb03d6b475010bd951f906fad748
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-04-14 18:25:57 -0500

    Refactoriza clase 'Empleado' para definir modelo de datos en Angular
    Aclaración:
    La forma tradicional de definir las propiedades de una clase con
    TypeScript funcionan para Angular. Sin embargo la forma propuesta
    requiere menos código y es más limpia, clara y fácil de mantener

--- src/app/empleado/empleado.ts
+++ src/app/empleado/empleado.ts
@@ -1,16 +1,10 @@
 /* Esta clase representa el modelo de datos (models) */
 export class Empleado {
-    /* Propiedades (Atributos) */
-    public nombre: string;
-    public edad: number;
-    public cargo: string;
-    public contratado: boolean;
-
     /* Constructor */
-    public constructor( nombre, edad ) {
-        this .nombre = nombre;
-        this .edad = edad;
-        this .cargo = cargo;
-        this .contratado = contratado;
-    }
+    public constructor(
+        public nombre: string,
+        public edad: number,
+        public cargo: string,
+        public contratado: boolean
+    ) {}
 }
